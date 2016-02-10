227a998fe00a614b71564b7e28eafc1f0544f5df
s525078 <s525078@mail.nwmissouri.edu>
2016-02-09 23:50:32 -0600

    Added CouponProfile

--- src/D05/Models/CouponProfile.cs
+++ src/D05/Models/CouponProfile.cs
@@ -8,11 +8,21 @@ namespace D05.Models
 {
     public class CouponProfile
     {
-        [ScaffoldColumn (false)]
-        [Key]public int ProfileID { get; set; }
 
         [ScaffoldColumn (false)]
+        [Required]
+        [Key]public int ProfileID { get; set; }
+
+       
+        [ScaffoldColumn (false)]
+        [Required]
         public int CouponID { get; set; }
+
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
 
         public bool isUsed { get; set; }
         
