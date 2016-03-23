7b189df5304efb347731d8937ca03d8497882947
sai sharanya <s522573@mail.nwmissouri.edu>
2016-03-23 17:30:00 -0500

    added changes related to tab navigations

--- src/D05/wwwroot/js/data.js
+++ src/D05/wwwroot/js/data.js
@@ -3,20 +3,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var user = [{
-        "username" : "bearcat",
-        "firstname" : "Bearcat",
-        "lastname" : "Bearcat",
-        "password" : "bearcat",
-        "email" : "bearcat@gmail.com",
-        "contact" : "9071234987",
-        
-},{
-        "username" : "chardy",
-        "firstname" : "Carolyn",
-        "lastname" : "Hardy",
-        "password" : "chardy",
-        "email" : "chardy@gmail.com",
-        "contact" : "9071234987",  
-}]
 
