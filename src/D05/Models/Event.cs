452774049d3f61e5c4e39d25e78682832abfe661
sai sharanya <s522573@mail.nwmissouri.edu>
2016-03-23 15:12:42 -0500

    Added Event model

---
+++ src/D05/Models/Event.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace D05.Models
+{
+    public class Event
+    {
+        [ScaffoldColumn(false)]
+        public int EventID { get; set; }
+
+        [Required]
+        [Display(Name = "Event Name")]
+        public string EventName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+        [Required]
+        [Display(Name = "Adult Price")]
+        [Range(typeof(decimal), "50.00", "100.00")]
+        public decimal AdultPrice { get; set; }
+
+
+        [Required]
+        [Display(Name = "Child Price")]
+        [Range(typeof(decimal), "10.00", "20.00")]
+        public decimal ChildPrice { get; set; }
+
+
+        [Required]
+        [Display(Name = "OldAge Price")]
+        [Range(typeof(decimal), "5.00", "10.99")]
+        public decimal OldAgePrice { get; set; }
+
+
+        [Required]
+        [Display(Name = "SpecialEvent")]
+        public bool IsSpecialEvent { get; set; }
+
+
+        [Required]
+        [Display(Name = "EventDate")]
+        public DateTime EventDate { get; set; }
+
+
+        [Required]
+        [Display(Name = "EventStartTime")]
+        public string EventStartTime { get; set; }
+
+
+        [Required]
+        [Display(Name = "EventStartTime")]
+        public string EventEndTime { get; set; }
+    }
+}
