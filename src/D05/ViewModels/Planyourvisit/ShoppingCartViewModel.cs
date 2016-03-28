3fc1d396c0d8d0d0f1dd3af4c873bd8f1137a8ca
sai sharanya <s522573@mail.nwmissouri.edu>
2016-03-28 12:10:19 -0500

    Added a new view model for Ticket Booking - beta release

---
+++ src/D05/ViewModels/Planyourvisit/ShoppingCartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D05.Models;
+
+namespace D05.ViewModels.Planyourvisit
+{
+    public class ShoppingCartViewModel
+    {
+        public OrderDetail OrderDetail { get; set; }
+
+        public Price Price { get; set; }
+
+        public List<Event> Event { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
