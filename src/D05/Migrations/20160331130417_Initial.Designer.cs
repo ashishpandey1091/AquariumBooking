db35d28617b8b8fe536c143fa0f81549081835c7
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2016-03-31 12:14:26 -0500

    Added milestones for team members

--- src/D05/Migrations/20160331130417_Initial.Designer.cs
+++ src/D05/Migrations/20160331130417_Initial.Designer.cs
@@ -8,9 +8,10 @@ using D05.Models;
 namespace D05.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160331130417_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
