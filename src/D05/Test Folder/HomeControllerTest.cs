77e6fc82b484192f731744e8f4a9781e4a31cda7
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2016-04-17 23:04:58 -0500

    HomeControllerTest.cs created with 10 test cases.

---
+++ src/D05/Test Folder/HomeControllerTest.cs
@@ -0,0 +1,200 @@
+﻿using D05.Controllers;
+using D05.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace D05.Test_Folder
+{
+    public class HomeControllerTest
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        // create a constructor to configure services
+        public HomeControllerTest()
+        {
+            var efServiceProvider = new ServiceCollection();
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+            .AddInMemoryDatabase()
+            .AddDbContext<ApplicationDbContext>(
+options => options.UseInMemoryDatabase());
+
+            _serviceProvider = services.BuildServiceProvider();
+
+        }
+
+        [Fact]
+        public void About_CreatesView()
+        {  // return type is void not async task
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);  // home controller doesn't use dbcontext
+
+            // Act
+            var result = controller.About(); // controller uses sync methods (not async)
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+
+        }
+
+
+        [Fact]
+        public void Contact_CreatesViewWithMessage()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);  // home controller doesn't use dbcontext
+
+            // Act
+            var result = controller.Contact();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotSame("Your contact page", viewResult.ViewData["Message"]);
+
+        }
+
+        [Fact]
+        public void About_CreatesWithMessage()
+        {
+            //Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);
+
+            //Act
+            var result = controller.Contact();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotSame("Your about page", viewResult.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void Contact_CreatesView()
+        {
+            //Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);
+
+            //Act
+            var result = controller.Contact();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+        }
+
+        [Fact]
+        public void FAQs_CreatesView()
+        {
+            //Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);
+
+            //Act
+            var result = controller.FAQs();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+        }
+
+        [Fact]
+        public void FAQs_CreatesViewWithMessage()
+        {
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);  // home controller doesn't use dbcontext
+
+            // Act
+            var result = controller.FAQs();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotSame("Your FAQs page", viewResult.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void TeamRoles_CreatesView()
+        {
+            //Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);
+
+            //Act
+            var result = controller.TeamRoles();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+        }
+
+        [Fact]
+        public void TeamRoles_CreatesViewWithMessage()
+        {
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);  // home controller doesn't use dbcontext
+
+            // Act
+            var result = controller.TeamRoles();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotSame("Your Team Roles page", viewResult.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void Milestones_CreatesView()
+        {
+            //Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);
+
+            //Act
+            var result = controller.Milestone();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+        }
+
+        [Fact]
+        public void Milestone_CreatesViewWithMessage()
+        {
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new HomeController(dbContext);  // home controller doesn't use dbcontext
+
+            // Act
+            var result = controller.Milestone();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.NotSame("Your Milestones page", viewResult.ViewData["Message"]);
+        }
+    }
+}
