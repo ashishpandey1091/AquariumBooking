2b476bac1b9d1eb151fac74d7d64d26da7009caf
sai sharanya <s522573@mail.nwmissouri.edu>
2016-04-18 01:49:02 -0500

    Added SchoolTripControllerTest

--- src/D05/Test Folder/SchoolTripsControllerTest.cs
+++ src/D05/Test Folder/SchoolTripsControllerTest.cs
@@ -1,23 +1,23 @@
-﻿using D05.Controllers;
-using D05.Models;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Data.Entity;
-using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using D05.Controllers;
+using D05.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace D05.Test_Folder
 {
-    public class BirthdayPartiesControllerTest
+    public class SchoolTripsControllerTest
     {
 
         // use dependency injection - create a local IServiceProvider
         private readonly IServiceProvider _serviceProvider;
 
-        public BirthdayPartiesControllerTest()
+        public SchoolTripsControllerTest()
         {
             var efServiceProvider = new ServiceCollection();
             var services = new ServiceCollection();
@@ -31,60 +31,38 @@ namespace D05.Test_Folder
         }
 
 
-        private static DbSet<BirthdayParty> CreateTestBirthdayParties(int numberOfLocations, ApplicationDbContext dbContext)
+        private static DbSet<SchoolTrip> CreateTestSchoolTrips(int numberOfLocations, ApplicationDbContext dbContext)
         {
 
-            var BirthdayParties = Enumerable.Range(1, numberOfLocations).Select(n =>
-                 new BirthdayParty()
+            var SchoolTrips = Enumerable.Range(1, numberOfLocations).Select(n =>
+                 new SchoolTrip()
                  {
-                     AttendeesNumber = 10+n,
-                     BirthdayPersonAge = 15+n,
-                     BirthdayPersonName = "king"+n,
+                     NoOfChildren = 5 + n,
+                     NoOfAdults = 10 + n,
+                     SchoolName = "School" + n,
                      ContactNumber = "1234567890",
                      DateofRegistration = new DateTime(2016, 1, 1),
                      DateofVisit = new DateTime(2016, 1, 1),
-                     FirstName = "Naruto"+n,
-                     FoodDetails = "nothing"+n,
-                     IsAdminAccepted = "Y" ,
-                     LastName = "Hinata",
-                     UserEmail = "something"+n+"@somthing.com"
+                     SchoolAddress = "Ohio" + n,
+                     IsAdminAccepted = "Y",
+                     SchoolEmail = "school" + n + "@gmail.com"
                  });
 
-            dbContext.AddRange(BirthdayParties);
+            dbContext.AddRange(SchoolTrips);
             dbContext.SaveChanges();
-            return dbContext.BirthdayParties;
+            return dbContext.SchoolTrips;
         }
 
 
         [Fact]
-        public void Events_Return_IndexView_NotNullWithTrueAssert()
+        public void Trips_Return_IndexView_NotNullWithTrueAssert()
         {
 
             // Arrange
-            var numDummyObjects = 3;
+            var numDummyObjects = 4;
             var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
-            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
-            var controller = new BirthdayPartiesController(dbContext);
-
-            // Act
-            var result = controller.Index();
-
-            // Assert with checking Type of data.
-            Assert.IsAssignableFrom<ViewResult>(result);
-
-
-
-        }
-
-        [Fact]
-        public void Events_Return_IndexView_NotNull_With_False_Assert()
-        {
-
-            // Arrange
-            var numDummyObjects = 3;
-            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
-            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
-            var controller = new BirthdayPartiesController(dbContext);
+            CreateTestSchoolTrips(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new SchoolTripsController(dbContext);
 
             // Act
             var result = controller.Index();
@@ -94,16 +72,34 @@ namespace D05.Test_Folder
 
         }
 
-
         [Fact]
-        public void Events_Return_DetailView_NOTNull()
+        public void Trips_Return_IndexView_NotNull_With_False_Assert()
         {
 
             // Arrange
             var numDummyObjects = 3;
             var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
-            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
-            var controller = new BirthdayPartiesController(dbContext);
+            CreateTestSchoolTrips(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new SchoolTripsController(dbContext);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert with checking Type of data.
+            Assert.IsAssignableFrom<ViewResult>(result);
+
+        }
+
+
+        [Fact]
+        public void Trips_Return_DetailView_NOTNull()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestSchoolTrips(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new SchoolTripsController(dbContext);
 
             // Act
             var result = controller.Details(1);
@@ -113,6 +109,8 @@ namespace D05.Test_Folder
 
         }
 
+
+
         [Fact]
         public void Events_Return_EditView_NOTNull()
         {
@@ -120,8 +118,8 @@ namespace D05.Test_Folder
             // Arrange
             var numDummyObjects = 3;
             var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
-            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
-            var controller = new BirthdayPartiesController(dbContext);
+            CreateTestSchoolTrips(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new SchoolTripsController(dbContext);
 
             // Act
             var result = controller.Edit(1);
@@ -136,13 +134,13 @@ namespace D05.Test_Folder
         {
 
             // Arrange
-            var numDummyObjects = 3;
+            var numDummyObjects = 4;
             var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
-            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
-            var controller = new BirthdayPartiesController(dbContext);
+            CreateTestSchoolTrips(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new SchoolTripsController(dbContext);
 
             // Act
-            var result = dbContext.BirthdayParties.Count<BirthdayParty>();
+            var result = dbContext.SchoolTrips.Count<SchoolTrip>();
 
             // Assert with checking Type of data.
             Assert.Equal(numDummyObjects, result);
@@ -151,7 +149,9 @@ namespace D05.Test_Folder
 
         }
 
-    }
+
+    
 
 
+}
 }
