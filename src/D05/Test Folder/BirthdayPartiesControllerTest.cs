4195d0f5d2d1ab49bf0abde0ac6c3f096487e1a8
Vijay Gopi <S522581@mail.nwmissouri.edu>
2016-04-18 00:15:12 -0500

    Unit tests for the View of Birthday party and Exhibits are done successful.

---
+++ src/D05/Test Folder/BirthdayPartiesControllerTest.cs
@@ -0,0 +1,157 @@
+﻿using D05.Controllers;
+using D05.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace D05.Test_Folder
+{
+    public class BirthdayPartiesControllerTest
+    {
+
+        // use dependency injection - create a local IServiceProvider
+        private readonly IServiceProvider _serviceProvider;
+
+        public BirthdayPartiesControllerTest()
+        {
+            var efServiceProvider = new ServiceCollection();
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+            .AddInMemoryDatabase()
+            .AddDbContext<ApplicationDbContext>(
+            options => options.UseInMemoryDatabase());
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+
+        private static DbSet<BirthdayParty> CreateTestBirthdayParties(int numberOfLocations, ApplicationDbContext dbContext)
+        {
+
+            var BirthdayParties = Enumerable.Range(1, numberOfLocations).Select(n =>
+                 new BirthdayParty()
+                 {
+                     AttendeesNumber = 10+n,
+                     BirthdayPersonAge = 15+n,
+                     BirthdayPersonName = "king"+n,
+                     ContactNumber = "1234567890",
+                     DateofRegistration = new DateTime(2016, 1, 1),
+                     DateofVisit = new DateTime(2016, 1, 1),
+                     FirstName = "Naruto"+n,
+                     FoodDetails = "nothing"+n,
+                     IsAdminAccepted = "Y" ,
+                     LastName = "Hinata",
+                     UserEmail = "something"+n+"@somthing.com"
+                 });
+
+            dbContext.AddRange(BirthdayParties);
+            dbContext.SaveChanges();
+            return dbContext.BirthdayParties;
+        }
+
+
+        [Fact]
+        public void Events_Return_IndexView_NotNullWithTrueAssert()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new BirthdayPartiesController(dbContext);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert with checking Type of data.
+            Assert.IsAssignableFrom<ViewResult>(result);
+
+
+
+        }
+
+        [Fact]
+        public void Events_Return_IndexView_NotNull_With_False_Assert()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new BirthdayPartiesController(dbContext);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert with checking Type of data.
+            Assert.IsAssignableFrom<ViewResult>(result);
+
+        }
+
+
+        [Fact]
+        public void Events_Return_DetailView_NOTNull()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new BirthdayPartiesController(dbContext);
+
+            // Act
+            var result = controller.Details(1);
+
+            // Assert with checking Type of data.
+            Assert.False(result == null);
+
+        }
+
+        [Fact]
+        public void Events_Return_EditView_NOTNull()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new BirthdayPartiesController(dbContext);
+
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert with checking Type of data.
+            Assert.IsAssignableFrom<ViewResult>(result);
+
+        }
+
+        [Fact]
+        public void Events_Return_List()
+        {
+
+            // Arrange
+            var numDummyObjects = 3;
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            CreateTestBirthdayParties(numberOfLocations: numDummyObjects, dbContext: dbContext);
+            var controller = new BirthdayPartiesController(dbContext);
+
+            // Act
+            var result = dbContext.BirthdayParties.Count<BirthdayParty>();
+
+            // Assert with checking Type of data.
+            Assert.Equal(numDummyObjects, result);
+            Assert.False(result < numDummyObjects);
+            Assert.True(result == numDummyObjects);
+
+        }
+
+    }
+
+
+}
---
+++ src/D05/Test Folder/ExhibitsControllerTest.cs
@@ -0,0 +1,133 @@
+﻿using D05.Controllers;
+using D05.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+using Xunit;
+
+namespace D05.Test_Folder
+{
+    public class ExhibitsControllerTest
+    {
+        // use dependency injection - create a local IServiceProvider
+        private readonly IServiceProvider _serviceProvider;
+
+        public ExhibitsControllerTest()
+        {
+            var efServiceProvider = new ServiceCollection();
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+            .AddInMemoryDatabase()
+            .AddDbContext<ApplicationDbContext>(
+            options => options.UseInMemoryDatabase());
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void Events_ReturnExhbitView()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new ExhibitsController(dbContext);
+
+            // Act
+            var result = controller.Exhibits("exone");
+
+            // Assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+
+        }
+
+
+
+        [Fact]
+        public void Events_ReturnView_NotNull()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new ExhibitsController(dbContext);
+
+            // Act
+            var result = controller.Exhibits("exone");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+        }
+
+        [Fact]
+        public void Events_ReturnView_WithViewData()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new ExhibitsController(dbContext);
+
+            // Act
+            var result = controller.Exhibits("exone");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsAssignableFrom<ViewDataDictionary>(viewResult.ViewData);
+            //Assert.Same("exone", viewResult.ViewData["key"]);
+            //Assert.Same("Your contact page.", viewResult.ViewData["Message"]);
+
+
+        }
+
+        [Fact]
+        public void Events_ReturnView_NotNull_ViewData_Notnull()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new ExhibitsController(dbContext);
+
+            // Act
+            var result = controller.Exhibits("exone");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+           
+            Assert.NotNull(viewResult.ViewData);
+            
+           
+
+
+        }
+
+        [Fact]
+        public void Events_ReturnView_ViewData_CorrectValue()
+        {
+
+            // Arrange
+            var dbContext = _serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var controller = new ExhibitsController(dbContext);
+
+            // Act
+            var result = controller.Exhibits("exone");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same("exone", viewResult.ViewData["key"]);
+            var data = viewResult.GetType();
+            Console.WriteLine(data + " I'm here");
+            //Assert.Same("Your contact page.", viewResult.ViewData["Message"]);
+
+
+        }
+
+       
+
+
+
+
+    }
+}
